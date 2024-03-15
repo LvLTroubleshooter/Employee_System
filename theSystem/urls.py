a237ee6312be6c15b6d2135ca84c224ae7b59865
LvLTroubleshooter <ismailliam144@gmail.com>
2024-03-15 19:38:57 +0800

    The App is Working fine having CRUD feature

--- theSystem/urls.py
+++ theSystem/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.employees_list, name='employees_list')
+    path('',views.employees_list, name='employees_list'),
+    path('create/', views.employee_create, name='employee_create'),
+    path('<int:pk>/update/', views.employee_update, name='employee_update'),
+    path('<int:pk>/delete/', views.employee_delete, name='employee_delete'),
 ]
--- theSystem/views.py
+++ theSystem/views.py
@@ -6,3 +6,36 @@ from .forms import EmployeesForm
 def employees_list(request):
     employees = Employees.objects.all()
     return render(request, 'employeesList.html', {'employees': employees})
+
+def employee_create(request):
+    if request.method == "POST":
+        form = EmployeesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employees_list')
+    else:
+        form = EmployeesForm()
+    return render(request, 'employeesForm.html', {'form': form})
+
+def employee_update(request, pk):
+    employee = get_object_or_404(Employees, pk=pk)
+    if request.method == "POST":
+        form = EmployeesForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect(employees_list)
+    else:
+        form = EmployeesForm(instance=employee)
+    return render(request, 'employeesForm.html', {'form': form})
+
+def employee_delete(request, pk):
+    employee = get_object_or_404(Employees, pk=pk)
+    if request.method == "POST":
+        employee.delete()
+        return redirect('employees_list')
+    return render(request, 'employeeDelete.html', {'employee': employee})
+
+    
+
+
+
